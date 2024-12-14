343982c4730074a6dd69eb0e90ea6678d53fbe60
Viktor Dück <mail@viktordueck.de>
2024-12-14 10:10:41 +0000

    initial settings and format

--- scripts/upload-imdb-data.zsh
+++ scripts/upload-imdb-data.zsh
@@ -7,4 +7,4 @@ IMDB_DATA_DIR="${IMDB_DATA_DIR:A}"
 # Upload the IMDB files in the Data Download Directory to the
 # Google Cloud Storage bucket with which Dataform will load
 # the data into BigQuery
-gsutil mv "$IMDB_DATA_DIR/*.tsv" "gs://winter-data/imdb"
+gsutil mv "$IMDB_DATA_DIR/*.tsv" "gs://quickstart345-test/imdb"
